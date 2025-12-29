7b3fbc7f4b29f5701f3c1fc7521166a55a46a7c8
Abenezer <abitysank@gmail.com>
2025-12-29 10:46:26 -0500

    Update Home.tsx
    Implemented home pages using the useFetch hook, and the types needed for showing the categories in the homepage.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,23 @@
+import useFetch from "../hooks/useFetch";
+import type { CategoriesResponse } from "../types/api";
+
 export default function Home() {
-    return <h1>Hello, its home</h1>;
+    const { data, loading, error } = useFetch<CategoriesResponse>(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    return (
+        <div>
+            <h1>Categories</h1>
+
+            {data?.categories.map((category) => (
+                <div key={category.idCategory}>
+                    <h3>{category.strCategory}</h3>
+                </div>
+            ))}
+        </div>
+    );
 }
