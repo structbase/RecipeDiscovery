c1e95d5b0d5953fea6fe22fdcbe161ee1f32c65d
Abenezer <abitysank@gmail.com>
2025-12-28 23:20:37 -0500

    Router update
    update to App.tsx to setup working with nav and main.tsx to handle router properly.

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import Search from "./pages/Search";
 import RecipeDetail from "./pages/RecipeDetail";
 import Favorites from "./pages/Favorites";
 import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/category/:categoryName" element={<Category />} />
-            <Route path="/recipe/:recipeId" element={<RecipeDetail />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/category/:categoryName" element={<Category />} />
+                <Route path="/recipe/:recipeId" element={<RecipeDetail />} />
+                <Route path="/favorites" element={<Favorites />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
     );
 }
 
