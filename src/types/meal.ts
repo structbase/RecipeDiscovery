d283449c939d1c1cbd10ff64ac853dd15eabb127
Abenezer <abitysank@gmail.com>
2025-12-30 10:43:20 -0500

    Update meal.ts
    comments update to make it bit clear

--- src/types/meal.ts
+++ src/types/meal.ts
@@ -12,7 +12,10 @@ export interface MealDetail {
     strArea: string;
     strInstructions: string;
 
-    // MealDB ingredients & measures (1–20)
-    [key: `strIngredient${number}`]: string | null;
-    [key: `strMeasure${number}`]: string | null;
+    // Dynamic properties for ingredients and measures (1–20).
+    // `number` is used to create keys like `strIngredient1`, `strMeasure1`, etc.
+    // Each property can either be a string (ingredient/measure) or null if not present.
+
+    [key: `strIngredient${number}`]: string | null; // Ingredient at position `number`.
+    [key: `strMeasure${number}`]: string | null; // Measure for ingredient at position `number`.
 }
