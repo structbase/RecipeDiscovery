cc57ab34488092b31097b374a7da44da92ef4e80
Abenezer <abitysank@gmail.com>
2025-12-29 23:03:55 -0500

    Update meal.ts
    More types for recipe details page

--- src/types/meal.ts
+++ src/types/meal.ts
@@ -3,3 +3,16 @@ export interface MealSummary {
     strMeal: string;
     strMealThumb: string;
 }
+
+export interface MealDetail {
+    idMeal: string;
+    strMeal: string;
+    strMealThumb: string;
+    strCategory: string;
+    strArea: string;
+    strInstructions: string;
+
+    // MealDB ingredients & measures (1–20)
+    [key: `strIngredient${number}`]: string | null;
+    [key: `strMeasure${number}`]: string | null;
+}
