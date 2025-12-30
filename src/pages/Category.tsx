d387da2d3585a811aa20c3a0de8b6ff2b187e7a3
Abenezer <abitysank@gmail.com>
2025-12-29 22:13:00 -0500

    Update Category.tsx
    Rendering all the meals received from each category after clicking the category on the homepage.

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,3 +1,34 @@
+import { Link, useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import type { MealSummary } from "../types/meal";
+import type { MealsResponse } from "../types/api";
+
 export default function Category() {
-    return <h1>Hello, its Category</h1>;
+    const { categoryName } = useParams<{ categoryName: string }>();
+
+    const { data, loading, error } = useFetch<MealsResponse<MealSummary>>(
+        categoryName
+            ? `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+            : ""
+    );
+
+    if (loading) return <p>Loading meals...</p>;
+    if (error) return <p>Error loading meals.</p>;
+    if (!data || !data.meals) return <p>No meals found.</p>;
+
+    return (
+        <div>
+            <h1>{categoryName} Meals</h1>
+
+            <ul>
+                {data.meals.map((meal) => (
+                    <li key={meal.idMeal}>
+                        <Link to={`/recipe/${meal.idMeal}`}>
+                            {meal.strMeal}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
